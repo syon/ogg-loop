333a2c72fe8de7c191ee4c3177a921efe82decea
syon <syon.xiii@gmail.com>
2020-07-09 00:46:29 +0900

    :zap: DropZone

---
+++ components/DropZone.vue
@@ -0,0 +1,20 @@
+<template>
+  <VueFullScreenFileDrop @drop="onDrop" />
+</template>
+
+<script>
+import VueFullScreenFileDrop from 'vue-full-screen-file-drop'
+import 'vue-full-screen-file-drop/dist/vue-full-screen-file-drop.css'
+
+export default {
+  components: {
+    VueFullScreenFileDrop,
+  },
+  methods: {
+    onDrop(formData, files) {
+      console.log({ formData, files })
+      this.$store.commit('dropper/SET_Files', Array.from(files))
+    },
+  },
+}
+</script>
