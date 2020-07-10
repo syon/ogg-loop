25e3eff284eda1d75bc05e35d907eab9cac53f05
syon <syon.xiii@gmail.com>
2020-07-10 22:44:46 +0900

    :package: vue-shortkey

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [],
+  plugins: ['~/plugins/shortkey'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
---
+++ plugins/shortkey.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import shortkey from 'vue-shortkey'
+
+Vue.use(shortkey)
