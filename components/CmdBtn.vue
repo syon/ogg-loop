4ce9e392f75f4fc1d9b26fafe5eb901726f24e40
syon <syon.xiii@gmail.com>
2020-07-12 14:20:00 +0900

    :gem: CmdBtn

---
+++ components/CmdBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-shortkey="shortkey"
+        v-bind="attrs"
+        v-on="on"
+        @shortkey="handleCommand(10)"
+        @click="handleCommand(10)"
+      >
+        <slot />
+      </v-btn>
+    </template>
+    <slot name="tooltip" />
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    shortkey: { type: Array, default: () => [] },
+  },
+  methods: {
+    handleCommand() {
+      this.$emit('do')
+    },
+  },
+}
+</script>
