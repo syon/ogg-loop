faa200911392342d04a3243cf86c0e6a78d910e5
syon <syon.xiii@gmail.com>
2025-11-28 21:11:29 +0900

    WaveformTimeline

--- components/LoopEditor.vue
+++ components/LoopEditor.vue
@@ -7,8 +7,8 @@ import CmdBtn from '@/components/CmdBtn'
 import AudioControls from '@/components/AudioControls'
 import LoopInfo from '@/components/LoopInfo'
 import FileToolbar from '@/components/FileToolbar'
+import WaveformTimeline from '@/components/WaveformTimeline'
 import Ogg from '@/lib/Ogg'
-import Surf from '@/lib/Surf'
 
 const dropperStore = useDropperStore()
 
@@ -18,8 +18,7 @@ const audioprocess = ref(0)
 const zoomVal = ref(0)
 const volumeVal = ref(50)
 const speedVal = ref(1.0)
-const wavesurfer = ref(null)
-const region = ref({})
+const waveformRef = ref(null)
 const meta = ref({})
 const metaReady = ref(false)
 const loading = ref(false)
@@ -35,6 +34,8 @@ const gFileBuffer = computed(() => dropperStore.gFileBuffer)
 
 const currentSample = computed(() => Math.round(audioprocess.value * 44100))
 const currentTime = computed(() => formatTime(audioprocess.value))
+
+const region = ref({})
 
 const sampleStart = computed(() => {
   if (region.value) {
@@ -71,7 +72,7 @@ const looplengthTime = computed(() =>
   formatTime(looplengthSample.value / 44100),
 )
 const isPlaying = computed(
-  () => wavesurfer.value && wavesurfer.value.isPlaying(),
+  () => waveformRef.value && waveformRef.value.isPlaying(),
 )
 
 // Watchers
@@ -102,116 +103,54 @@ const applySampleAudio = async () => {
 
 const refresh = () => {
   myfile.value = gFile.value
-  load(gFileBuffer.value)
 }
 
-const load = (fileBuffer) => {
-  if (wavesurfer.value) {
-    wavesurfer.value.destroy()
-  }
-  const options = {
-    loopstart: meta.value.LOOPSTART,
-    looplength: meta.value.LOOPLENGTH,
-  }
-  wavesurfer.value = Surf.create(options)
-
-  // Wait for initial region to be created
-  wavesurfer.value.once('decode', () => {
-    region.value = wavesurfer.value.initialRegion
-    syncRegionToForm()
-
-    // Inject custom styles into Shadow DOM
-    injectShadowStyles()
-
-    // Set up region event listeners
-    if (region.value) {
-      region.value.on('update-end', () => {
-        syncRegionToForm()
-      })
-    }
-
-    // Set up loop playback handler using regions plugin
-    setupLoopHandler()
-  })
-
-  wavesurfer.value.on('audioprocess', (sec) => {
-    audioprocess.value = sec
-  })
-
-  wavesurfer.value.on('seeking', (sec) => {
-    audioprocess.value = sec
-  })
-
-  if (fileBuffer) {
-    wavesurfer.value.load(fileBuffer)
-  }
-
-  changeVolume()
+const handleRegionUpdate = (newRegion) => {
+  region.value = newRegion
+  syncRegionToForm()
 }
 
 const playPause = () => {
-  if (wavesurfer.value.isPlaying()) {
-    wavesurfer.value.pause()
-  } else {
-    // Start playing the region if loop is enabled
-    if (loop.value && region.value) {
-      region.value.play()
-    } else {
-      wavesurfer.value.play()
-    }
+  if (waveformRef.value) {
+    waveformRef.value.playPause()
   }
 }
 
 const handleSkip = (offset) => {
-  wavesurfer.value.skip(offset)
+  if (waveformRef.value) {
+    waveformRef.value.skip(offset)
+  }
 }
 
 const handleRepeat = (offset) => {
-  const sec = region.value.end - offset
-  wavesurfer.value.play(sec)
+  if (waveformRef.value && region.value) {
+    const sec = region.value.end - offset
+    waveformRef.value.play(sec)
+  }
 }
 
 const changeZoom = (sub) => {
   switch (sub) {
     case 'minus':
       zoomVal.value = zoomVal.value <= 20 ? 0 : zoomVal.value - 20
-      wavesurfer.value.zoom(Number(zoomVal.value))
       break
     case 'plus':
       zoomVal.value += 20
-      wavesurfer.value.zoom(Number(zoomVal.value))
       break
     default:
       zoomVal.value = 0
-      wavesurfer.value.zoom(0)
       break
   }
-}
-
-const changeVolume = () => {
-  wavesurfer.value.setVolume(Number(volumeVal.value / 100))
+  if (waveformRef.value) {
+    waveformRef.value.zoom(Number(zoomVal.value))
+  }
 }
 
 const changeSpeed = (v) => {
   speedVal.value = v
-  wavesurfer.value.setPlaybackRate(Number(v))
-}
-
-const setupLoopHandler = () => {
-  if (!wavesurfer.value || !wavesurfer.value.regionsPlugin) return
-
-  const regionsPlugin = wavesurfer.value.regionsPlugin
-
-  regionsPlugin.on('region-in', (reg) => {
-    console.log('region-in', reg)
-  })
-
-  regionsPlugin.on('region-out', (reg) => {
-    console.log('region-out', reg)
-    if (loop.value) {
-      reg.play()
-    }
-  })
+  if (waveformRef.value) {
+    waveformRef.value.setPlaybackRate(Number(v))
+  }
 }
 
 const syncRegionToForm = () => {
@@ -222,12 +161,12 @@ const syncRegionToForm = () => {
 }
 
 const syncFormToRegion = () => {
-  if (!region.value) return
+  if (!region.value || !waveformRef.value) return
   const start = Number(formLoopStartSample.value) / 44100
   const end =
     (Number(formLoopStartSample.value) + Number(formLoopLengthSample.value)) /
     44100
-  region.value.setOptions({ start, end })
+  waveformRef.value.updateRegion(start, end)
 }
 
 const handleScanOgg = async () => {
@@ -257,62 +196,6 @@ const handleSubmit = async () => {
     alert('Write Error.')
   }
   loading.value = false
-}
-
-const injectShadowStyles = () => {
-  // Find the waveform container
-  const waveformContainer = document.querySelector('#waveform')
-  if (!waveformContainer) return
-
-  // Try finding shadow DOM containers
-  const canvasElements = waveformContainer.querySelectorAll('div')
-  canvasElements.forEach((el) => {
-    if (el.shadowRoot) {
-      addStylesToShadowRoot(el.shadowRoot)
-    }
-  })
-
-  // Directly target elements with part attribute using partial match (~=)
-  const regionElements = waveformContainer.querySelectorAll('[part~="region"]')
-  regionElements.forEach((el) => {
-    el.style.height = '50%'
-    el.style.zIndex = '9'
-  })
-
-  const handleElements = waveformContainer.querySelectorAll(
-    '[part~="region-handle"]',
-  )
-  handleElements.forEach((el) => {
-    el.style.backgroundColor = '#f57f17'
-    el.style.width = '2px'
-  })
-
-  // Add global styles using ::part() and attribute selectors
-  const styleElement = document.createElement('style')
-  styleElement.textContent = `
-    ::part(region) {
-      height: 50% !important;
-      z-index: 9 !important;
-    }
-    ::part(region-handle) {
-      border-color: #f57f17 !important;
-    }
-  `
-  document.head.appendChild(styleElement)
-}
-
-const addStylesToShadowRoot = (shadowRoot) => {
-  const style = document.createElement('style')
-  style.textContent = `
-    ::part(region) {
-      height: 50% !important;
-      z-index: 9 !important;
-    }
-    ::part(region-handle) {
-      border-color: #f57f17 !important;
-    }
-  `
-  shadowRoot.appendChild(style)
 }
 
 // Lifecycle
@@ -383,11 +266,19 @@ onMounted(async () => {
       />
     </div>
 
-    <div class="my-4" style="position: relative">
-      <div id="waveform-timeline"></div>
-      <div id="waveform"></div>
-      <div id="waveform-minimap"></div>
-    </div>
+    <WaveformTimeline
+      ref="waveformRef"
+      :file-buffer="gFileBuffer"
+      :loopstart="meta.LOOPSTART"
+      :looplength="meta.LOOPLENGTH"
+      :loop="loop"
+      :volume-val="volumeVal"
+      @update:region="handleRegionUpdate"
+      @audioprocess="audioprocess = $event"
+      @seeking="audioprocess = $event"
+      @region-update-end="syncRegionToForm"
+    />
+
     <v-overlay :value="loading">
       <v-progress-circular indeterminate size="64"></v-progress-circular>
     </v-overlay>
---
+++ components/WaveformTimeline.vue
@@ -0,0 +1,247 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import Surf from '@/lib/Surf'
+
+const props = defineProps({
+  fileBuffer: {
+    type: String,
+    default: null,
+  },
+  loopstart: {
+    type: Number,
+    default: null,
+  },
+  looplength: {
+    type: Number,
+    default: null,
+  },
+  loop: {
+    type: Boolean,
+    default: true,
+  },
+  volumeVal: {
+    type: Number,
+    default: 50,
+  },
+})
+
+const emit = defineEmits([
+  'update:region',
+  'audioprocess',
+  'seeking',
+  'region-update-end',
+])
+
+const wavesurfer = ref(null)
+const region = ref({})
+
+// Watch for file buffer changes
+watch(
+  () => props.fileBuffer,
+  (newBuffer) => {
+    if (newBuffer) {
+      loadWaveform(newBuffer)
+    }
+  },
+)
+
+// Watch for volume changes
+watch(
+  () => props.volumeVal,
+  (newVolume) => {
+    if (wavesurfer.value) {
+      wavesurfer.value.setVolume(Number(newVolume / 100))
+    }
+  },
+)
+
+const loadWaveform = (fileBuffer) => {
+  if (wavesurfer.value) {
+    wavesurfer.value.destroy()
+  }
+
+  const options = {
+    loopstart: props.loopstart,
+    looplength: props.looplength,
+  }
+  wavesurfer.value = Surf.create(options)
+
+  // Wait for initial region to be created
+  wavesurfer.value.once('decode', () => {
+    region.value = wavesurfer.value.initialRegion
+    emit('update:region', region.value)
+
+    // Inject custom styles into Shadow DOM
+    injectShadowStyles()
+
+    // Set up region event listeners
+    if (region.value) {
+      region.value.on('update-end', () => {
+        emit('region-update-end', region.value)
+      })
+    }
+
+    // Set up loop playback handler using regions plugin
+    setupLoopHandler()
+  })
+
+  wavesurfer.value.on('audioprocess', (sec) => {
+    emit('audioprocess', sec)
+  })
+
+  wavesurfer.value.on('seeking', (sec) => {
+    emit('seeking', sec)
+  })
+
+  if (fileBuffer) {
+    wavesurfer.value.load(fileBuffer)
+  }
+
+  // Set initial volume
+  wavesurfer.value.setVolume(Number(props.volumeVal / 100))
+}
+
+const setupLoopHandler = () => {
+  if (!wavesurfer.value || !wavesurfer.value.regionsPlugin) return
+
+  const regionsPlugin = wavesurfer.value.regionsPlugin
+
+  regionsPlugin.on('region-in', (reg) => {
+    console.log('region-in', reg)
+  })
+
+  regionsPlugin.on('region-out', (reg) => {
+    console.log('region-out', reg)
+    if (props.loop) {
+      reg.play()
+    }
+  })
+
+  console.log('Loop handler set up')
+}
+
+const injectShadowStyles = () => {
+  // Find the waveform container
+  const waveformContainer = document.querySelector('#waveform')
+  if (!waveformContainer) return
+
+  // Try finding shadow DOM containers
+  const canvasElements = waveformContainer.querySelectorAll('div')
+  canvasElements.forEach((el) => {
+    if (el.shadowRoot) {
+      addStylesToShadowRoot(el.shadowRoot)
+    }
+  })
+
+  // Directly target elements with part attribute using partial match (~=)
+  const regionElements = waveformContainer.querySelectorAll('[part~="region"]')
+  regionElements.forEach((el) => {
+    el.style.height = '50%'
+    el.style.zIndex = '9'
+  })
+
+  const handleElements = waveformContainer.querySelectorAll(
+    '[part~="region-handle"]',
+  )
+  handleElements.forEach((el) => {
+    el.style.backgroundColor = '#f57f17'
+    el.style.width = '2px'
+  })
+
+  // Add global styles using ::part() and attribute selectors
+  const styleElement = document.createElement('style')
+  styleElement.textContent = `
+    ::part(region) {
+      height: 50% !important;
+      z-index: 9 !important;
+    }
+    ::part(region-handle) {
+      border-color: #f57f17 !important;
+    }
+  `
+  document.head.appendChild(styleElement)
+}
+
+const addStylesToShadowRoot = (shadowRoot) => {
+  const style = document.createElement('style')
+  style.textContent = `
+    ::part(region) {
+      height: 50% !important;
+      z-index: 9 !important;
+    }
+    ::part(region-handle) {
+      border-color: #f57f17 !important;
+    }
+  `
+  shadowRoot.appendChild(style)
+}
+
+// Public methods exposed to parent
+const playPause = () => {
+  if (wavesurfer.value) {
+    if (wavesurfer.value.isPlaying()) {
+      wavesurfer.value.pause()
+    } else {
+      // Start playing the region if loop is enabled
+      if (props.loop && region.value) {
+        region.value.play()
+      } else {
+        wavesurfer.value.play()
+      }
+    }
+  }
+}
+
+const skip = (offset) => {
+  if (wavesurfer.value) {
+    wavesurfer.value.skip(offset)
+  }
+}
+
+const play = (sec) => {
+  if (wavesurfer.value) {
+    wavesurfer.value.play(sec)
+  }
+}
+
+const zoom = (value) => {
+  if (wavesurfer.value) {
+    wavesurfer.value.zoom(Number(value))
+  }
+}
+
+const setPlaybackRate = (rate) => {
+  if (wavesurfer.value) {
+    wavesurfer.value.setPlaybackRate(Number(rate))
+  }
+}
+
+const isPlaying = () => {
+  return wavesurfer.value && wavesurfer.value.isPlaying()
+}
+
+const updateRegion = (start, end) => {
+  if (region.value) {
+    region.value.setOptions({ start, end })
+  }
+}
+
+// Expose public methods to parent
+defineExpose({
+  playPause,
+  skip,
+  play,
+  zoom,
+  setPlaybackRate,
+  isPlaying,
+  updateRegion,
+})
+</script>
+
+<template>
+  <div class="my-4" style="position: relative">
+    <div id="waveform-timeline"></div>
+    <div id="waveform"></div>
+    <div id="waveform-minimap"></div>
+  </div>
+</template>
